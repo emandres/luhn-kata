727df5650d001000ea7b68c33c2dd4e7176676c4
Eric Andres <emandres@gmail.com>
2015-03-03 17:56:19 -0700

    Initial example commit

---
+++ example/LuhnCheckerExample/LuhnChecker.fs
@@ -0,0 +1,7 @@
+﻿namespace LuhnCheckerExample
+
+open System
+
+module LuhnChecker =
+    let foo = "bar"
+
---
+++ example/LuhnCheckerExample/Tests.fs
@@ -0,0 +1,10 @@
+﻿namespace LuhnCheckerExample
+
+open NUnit.Framework
+open LuhnChecker
+
+[<TestFixture>]
+type Tests() =
+    [<Test>]
+    member this.dummy() =
+        Assert.Inconclusive()
