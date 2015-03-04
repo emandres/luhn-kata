adf000fe0c61e89e468506e967de26055d5523c4
Eric Andres <emandres@gmail.com>
2015-03-03 22:32:16 -0700

    Step 4.3

--- example/LuhnKataExample/LuhnCheck.fs
+++ example/LuhnKataExample/LuhnCheck.fs
@@ -8,5 +8,10 @@ module LuhnCheck =
     let evenDigitValue digit =
         2 * digit
 
+    let digits number =
+       number.ToString() |>
+       Seq.map (fun c -> Int32.Parse((string)c))
+
     let sumDigits number =
-       1
+       digits number |>
+       Seq.sum
